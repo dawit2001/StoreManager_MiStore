990c8eaada43278b879c5ea64cbf7c4b1f1a465f
dawit2001 <abebedawit2001@gmail.com>
2023-07-11 01:19:32 +0300

    added dependencies

--- server/server.js
+++ server/server.js
@@ -1,7 +1,46 @@
+import crypto from "crypto";
 import express from "express";
+import session from "express-session";
+import cookieParser from "cookie-parser";
 const app = express();
-const port = 8000;
+const port = 8001;
+const randomString = crypto.randomBytes(32).toString("hex");
+app.use(cookieParser());
+app.use(
+  session({
+    secret: randomString,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+console.log(randomString);
 
+app.get("/", (req, res) => {
+  req.session.username = "dawit wondwosen";
+  res.send(req.session.username);
+  // res.send("session setted");
+});
+app.get("/dashboard", (req, res) => {
+  res.send(`welcome Mr.${req.session.username}`);
+});
 app.listen(port, () => {
   console.log("running on port " + port);
 });
+// import jwt from "jsonwebtoken";
+// const payload = {
+//   userId: 33,
+//   Username: "dawit",
+//   Role: "admin",
+// };
+// console.log(randomString);
+// const token = jwt.sign(payload, randomString, { expiresIn: "1h" });
+// console.log("generated token", token);
+
+// jwt.verify(token, randomString, (err, decode) => {
+//   if (err) {
+//     console.log("error while decoding");
+//     return;
+//   }
+//   console.log("Decoded payload", decode);
+// });
